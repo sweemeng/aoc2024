7db39f29b7fecf508525519e634c9fadd577c8a1
sweemeng <sweester@gmail.com>
2024-12-04 07:48:22 +0800

    Steal answer from other for question 2.2. and clean up

--- day_02/main.py
+++ day_02/main.py
@@ -13,12 +13,12 @@ def loader(data_file):
             yield [int(i) for i in line.split()]
 
 
-def solution(data_file, safety=False, debug=False, failed_only=False):
+def solution(data_file, debug=False, triggered=False):
     data = loader(data_file)
     safe_count = 0
 
     for row in data:
-        check = check_data(row, debug=debug)
+        check = check_data(row, triggered=triggered)
         if debug:
             print("Row", row)
             print("Check", check)
@@ -27,61 +27,28 @@ def solution(data_file, safety=False, debug=False, failed_only=False):
     print(safe_count)
 
 
-def check_data(row, debug=DEBUG):
-    safe = True
-    trend = None
-    copies = deepcopy(row)
-    while copies:
-        if debug:
-            print(copies)
-        if len(copies) == 1:
-            break
-        d1 = copies.pop(0)
-        d2 = copies.pop(0)
-        if trend is None:
-            trend = get_trend(d1, d2)
-        safe = is_data_safe(d1, d2, trend, debug=debug)
-        if not safe:
-            break
-        trend = get_trend(d1, d2)
-        copies.insert(0, d2)
+def check_data(row, triggered=False):
+    diffs = []
+
+    for i in range(len(row) - 1):
+        diffs.append(row[i] - row[i+1])
+    check_1 = all([i in range(1, 4) for i in diffs])
+    check_2 = all([i in range(-3, 0) for i in diffs])
+    safe = check_1 or check_2
+    if not safe and not triggered:
+        for i in range(len(row)):
+            new_row = deepcopy(row)
+            new_row.pop(i)
+            safe = check_data(new_row, triggered=True)
+            if safe:
+                break
     return safe
-
-
-def is_data_safe(num_1, num_2, trends, debug=False):
-    safe = True
-    trend = get_trend(num_1, num_2)
-    if debug:
-        print("num 1", num_1)
-        print("num 2", num_2)
-        print("Trend", trend)
-    if trend == NEUTRAL:
-        if debug:
-            print("trend cannot be zero")
-        return False
-    if trend != trends and trends is not None:
-        if debug:
-            print("Trend mismatch")
-        safe = False
-
-    diff = abs(num_1 - num_2)
-    if debug:
-        print("Diff of", num_1, num_2, "is", diff)
-    if diff not in range(1, 4):
-        if debug:
-            print("Diff not in range", diff)
-        safe = False
-    return safe
-
-
-def get_trend(num_1, num_2):
-    diff = num_1 - num_2
-    if diff == 0:
-        return NEUTRAL
-    return abs(diff)/diff
 
 
 if __name__ == "__main__":
-    solution("day_02/test_data.txt", debug=True)
+    solution("day_02/test_data.txt", triggered=True)
+    solution("day_02/actual_data.txt", triggered=True)
+    solution("day_02/test_data.txt")
     solution("day_02/actual_data.txt")
-    # Current answer part 2, too low 337
+
+    # Current answer part 2, too low 337 not 348
